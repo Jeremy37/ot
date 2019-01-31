0554deb074de8809e1c9bde2f85d91ec2cc8d35b
js29 <js29@sanger.ac.uk>
2019-01-31 14:23:22 +0000

    Updated to use "replicate_full" field in input metadata file

--- experiment/getFlashInput.R
+++ experiment/getFlashInput.R
@@ -23,12 +23,12 @@ df = meta.df %>% dplyr::left_join(regions.df, by="name") %>%
   dplyr::rename(amplicon_size = end) %>%
   dplyr::mutate(read_len = 150, amplicon_sd = 20,
                 expected_overlap = 2*read_len - amplicon_size) %>%
-  dplyr::mutate(output_file = paste0(replicate, ".extendedFrags.fastq.gz"))
+  dplyr::mutate(output_file = paste0(replicate_full, ".extendedFrags.fastq.gz"))
 
 df$min_overlap = sapply(1:nrow(df), FUN = function(i) getMinOverlap(df$amplicon_size[i], df$read_len[i]))
 df$max_mismatch_dens = 0.05 + 1 / df$expected_overlap
 df$max_mismatch_dens = sprintf("%.3g", df$max_mismatch_dens)
 
-df = df %>% dplyr::select(replicate, Read_1_file, Read_2_file, read_len, amplicon_size, amplicon_sd, min_overlap, max_mismatch_dens, output_file)
+df = df %>% dplyr::select(replicate_full, Read_1_file, Read_2_file, read_len, amplicon_size, amplicon_sd, min_overlap, max_mismatch_dens, output_file)
 
 write.table(df, file="", sep="\t", quote=F, row.names=F, col.names=T)
